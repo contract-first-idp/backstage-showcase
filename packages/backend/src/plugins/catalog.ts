23579eca33feeac3b76b4144c7f3bf4766abb740
Jason Froehlich <38667521+jayfray12@users.noreply.github.com>
2023-11-07 15:38:24 -0500

    Update catalog.ts

--- packages/backend/src/plugins/catalog.ts
+++ packages/backend/src/plugins/catalog.ts
@@ -57,11 +57,7 @@ export default async function createPlugin(
       KeycloakOrgEntityProvider.fromConfig(env.config, {
         id: 'development',
         logger: env.logger,
-        schedule: env.scheduler.createScheduledTaskRunner({
-          frequency: { hours: 1 },
-          timeout: { minutes: 50 },
-          initialDelay: { seconds: 15 },
-        }),
+        scheduler: env.scheduler,
       }),
     );
   }
@@ -140,11 +136,7 @@ export default async function createPlugin(
     builder.addEntityProvider(
       MicrosoftGraphOrgEntityProvider.fromConfig(env.config, {
         logger: env.logger,
-        schedule: env.scheduler.createScheduledTaskRunner({
-          frequency: { hours: 1 },
-          timeout: { minutes: 50 },
-          initialDelay: { seconds: 15 },
-        }),
+        scheduler: env.scheduler,
       }),
     );
   }
